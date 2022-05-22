838c36e45d3974aaef42c3a3d4e6efc8ba2753a8
Ziadashraf98 <105677630+Ziadashraf98@users.noreply.github.com>
2022-05-22 10:17:25 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
 
 
 class HomeController extends Controller
@@ -94,6 +97,37 @@ class HomeController extends Controller
         $cart = Cart::find($id);
         $cart->delete();
         return back();
+    }
+
+    public function cash_order()
+    {
+        $user = Auth::user();
+        $userid = $user->id;
+        $data = Cart::where('user_id' , $userid)->get();
+        
+        foreach($data as $data)
+        {
+            $order = new Order;
+            $order->name = $data->name;
+            $order->email = $data->email;
+            $order->phone = $data->phone;
+            $order->address = $data->address;
+            $order->user_id = $data->user_id;
+            $order->product_title = $data->product_title;
+            $order->quantity = $data->quantity;
+            $order->price = $data->price;
+            $order->image = $data->image;
+            $order->product_id = $data->product_id;
+            $order->payment_status = 'cash on delivery';
+            $order->delivery_status = 'processing';
+            $order->save();
+        }
+        
+        DB::table('carts')->where('phone' , $user->phone)->delete();
+
+        return back()->with('message' , 'We have Received your Order. We will connect with you soon...');
+
+         
     }
 
 }
