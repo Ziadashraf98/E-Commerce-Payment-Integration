228c716b54201dee92b305b16409310e8fa8d45a
Ziadashraf98 <105677630+Ziadashraf98@users.noreply.github.com>
2022-05-30 12:39:00 +0200

    Update product_details.blade.php

--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -26,7 +26,15 @@
       <div class="hero_area">
          <!-- header section strats -->
         @include('home.header')
-     
+        @if(session()->has('message'))
+            
+        <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            {{session()->get('message')}}
+            
+        </div>
+        
+        @endif
 
       <div class="col-sm-6 col-md-4 col-lg-4" style="margin: auto; width: 50%; padding:30px;">
 
