1c7d44dddd4ba895b27df6d066c5823c3cff9060
Ziadashraf98 <105677630+Ziadashraf98@users.noreply.github.com>
2022-06-02 21:31:07 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -147,6 +147,30 @@ class HomeController extends Controller
                 "source" => $request->stripeToken,
                 "description" => "Thanks for payment." 
         ]);
+
+        $user = Auth::user();
+        $userid = $user->id;
+        $data = Cart::where('user_id' , $userid)->get();
+        
+        foreach($data as $data)
+        {
+            $order = new Order;
+            $order->name = $data->name;
+            $order->email = $data->email;
+            $order->phone = $data->phone;
+            $order->address = $data->address;
+            $order->user_id = $data->user_id;
+            $order->product_title = $data->product_title;
+            $order->quantity = $data->quantity;
+            $order->price = $data->price;
+            $order->image = $data->image;
+            $order->product_id = $data->product_id;
+            $order->payment_status = 'Paid';
+            $order->delivery_status = 'processing';
+            $order->save();
+        }
+        
+        DB::table('carts')->where('phone' , $user->phone)->delete();
       
         Session::flash('success', 'Payment successful!');
               
