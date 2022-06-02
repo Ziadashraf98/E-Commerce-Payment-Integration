349eabf2ecb7837936c9980b2a49b2bfa5c9faf5
Ziadashraf98 <105677630+Ziadashraf98@users.noreply.github.com>
2022-06-02 21:39:23 +0200

    Update stripe.blade.php

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -177,4 +177,12 @@ $(function() {
      
 });
 </script>
+
+<script src="home/js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="home/js/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="home/js/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="home/js/custom.js"></script>
 </html>
